1edf058adc9f2d90cd78715fcd216c77e469510e
Fernando Garcia <141573321+FeuGarcia@users.noreply.github.com>
2023-08-29 19:11:15 -0300

    Add files via upload

---
+++ CalcularImposto/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CalcularImposto;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Informe sua renda: ");
+        double renda = double.Parse(Console.ReadLine());
+        double imposto = Calcular.Calculo(renda);
+        Console.WriteLine($"O valor do imposto de renda é: {imposto.ToString("F2")}");
+    }
+}
